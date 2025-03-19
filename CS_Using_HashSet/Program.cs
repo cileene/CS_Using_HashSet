c0eba8cd493417ab9f7fb91b0496bc037cc61804
cileene <106276740+cileene@users.noreply.github.com>
2025-03-19 15:14:24 +0100

    Commit often

--- CS_Using_HashSet/Program.cs
+++ CS_Using_HashSet/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace CS_Using_HashSet
 {
@@ -121,11 +122,27 @@ namespace CS_Using_HashSet
             "Mahesh", "Ramesh", "Ram", "GundaRam", "Sabnis",
             "Leena", "Neema", "Sita"
         };
+        
+        static string[] _uniqueNames = Enumerable.Range(1, 500).Select(i => $"Name{i}").ToArray();
 
         static void Main(string[] args)
         {
+            // Check the length of our _uniqueNames array
+            Console.WriteLine(_uniqueNames.Length);
+            
+            // Print content of _uniqueNames
+            foreach (var name in _uniqueNames)
+            {
+                Console.WriteLine(name);
+            }
+            
+            
+            
+            // Section 2: Eliminating Duplicates in C# HashSet --------------------------------------------------------
+            
+            
             Console.WriteLine("Using HashSet");
-            //1. Defining String Array (Note that the string "mahesh" is repeted) 
+            //1. Defining String Array (Note that the string "mahesh" is repeated) 
             string[] names = new string[]
             {
                 "mahesh",
@@ -158,6 +175,10 @@ namespace CS_Using_HashSet
                 Console.WriteLine(n);
             }
 
+            
+            // Section 3: Modify HashSet Using UnionWith() Method -----------------------------------------------------
+            
+            
             //1.
             string[] names1 = new string[]
             {
@@ -168,6 +189,7 @@ namespace CS_Using_HashSet
             {
                 "suprotim", "agarwal", "vikram", "pendse", "mahesh", "mitkari"
             };
+            
             //2.
             HashSet<string> hSetN1 = new HashSet<string>(names1);
             Console.WriteLine("Data in First HashSet");
@@ -185,6 +207,7 @@ namespace CS_Using_HashSet
             }
 
             Console.WriteLine("________________________________________________________________");
+            
             //3.
             Console.WriteLine("Data After Union");
             hSetN1.UnionWith(hSetN2);
@@ -197,6 +220,7 @@ namespace CS_Using_HashSet
             Console.WriteLine("_________________________________");
             Console.WriteLine("Data in HashSet before using Except With");
             Console.WriteLine("_________________________________");
+            
             //storing data of hSetN3 in temporary HashSet
             HashSet<string> hSetN3 = new HashSet<string>(names1);
             foreach (var n in hSetN3)
@@ -241,8 +265,10 @@ namespace CS_Using_HashSet
                 Console.WriteLine(n);
             }
 
+            
+            // Section 6: Checking performance of operations like Add, Remove, Contains on HashSet vs List. -----------
+            
             Get_Add_Performance_HashSet_vs_List();
-
             Get_Contains_Performance_HashSet_vs_List();
             Get_Remove_Performance_HashSet_vs_List();
 
@@ -256,14 +282,12 @@ namespace CS_Using_HashSet
             Console.WriteLine();
             List<string> lstNames = new List<string>();
             var s2 = Stopwatch.StartNew();
-            foreach (string s in _names)
+            foreach (string s in _uniqueNames)
             {
                 lstNames.Add(s);
             }
 
             s2.Stop();
-            //Console.WriteLine(((double)(s2.Elapsed.TotalMilliseconds * 10000000) /
-            // 1000000).ToString("0.000 ns"));
             Console.WriteLine(s2.Elapsed.TotalMilliseconds.ToString("0.000 ms"));
             Console.WriteLine();
             Console.WriteLine("Ends Here");
@@ -274,21 +298,66 @@ namespace CS_Using_HashSet
 
             HashSet<string> hStringNames = new HashSet<string>(StringComparer.Ordinal);
             var s1 = Stopwatch.StartNew();
-            foreach (string s in _names)
+            foreach (string s in _uniqueNames)
             {
                 hStringNames.Add(s);
             }
 
             s1.Stop();
-            //Console.WriteLine(((double)(s1.Elapsed.TotalMilliseconds * 10000000) /
-            //  1000000).ToString("0.000 ns"));
             Console.WriteLine(s1.Elapsed.TotalMilliseconds.ToString("0.000 ms"));
             Console.WriteLine();
             Console.WriteLine("Ends Here");
             Console.WriteLine("____________________________________");
             Console.WriteLine();
         }
+        
+        // This prints:
+        /*
+            ____________________________________
+            List Performance while Adding Item
 
+            1,696 ms
+
+            Ends Here
+
+            ____________________________________
+            HashSet Performance while Adding Item
+
+            0,012 ms
+
+            Ends Here
+            ____________________________________
+            */
+        
+        // Using HashSets here is a lot faster, which appears to be an anomaly.
+        // Aaaaand now I figured it out.
+        // The data set supplied on the website contains mostly duplicates.
+        // Making the collection stored as a hashset way smaller.
+        // Thus contaminating the experiment.
+        // Now we have a new _uniqueNames array that through a handy function creates 500 unique strings.
+        // Name1, Name2, Name3, etc.
+        // And now we can run the experiment again using _uniqueNames instead of _names.
+        
+        // This prints:
+        /*  ____________________________________
+            List Performance while Adding Item
+
+            0,006 ms
+
+            Ends Here
+
+            ____________________________________
+            HashSet Performance while Adding Item
+
+            0,026 ms
+
+            Ends Here
+            ____________________________________
+            */
+        
+        // This is a lot more reasonable, and lists are now faster than hashsets.
+        // This is because the list can just add an element and be done.
+        // While the hashset has to check if the element already exists in the hashset.
 
         static void Get_Contains_Performance_HashSet_vs_List()
         {
@@ -297,14 +366,12 @@ namespace CS_Using_HashSet
             Console.WriteLine();
             List<string> lstNames = new List<string>();
             var s2 = Stopwatch.StartNew();
-            foreach (string s in _names)
+            foreach (string s in _uniqueNames)
             {
                 lstNames.Contains(s);
             }
 
             s2.Stop();
-            //Console.WriteLine(((double)(s2.Elapsed.TotalMilliseconds * 10000000) /
-            // 1000000).ToString("0.000 ns"));
             Console.WriteLine(s2.Elapsed.TotalMilliseconds.ToString("0.000 ms"));
             Console.WriteLine();
             Console.WriteLine("Ends Here");
@@ -315,20 +382,38 @@ namespace CS_Using_HashSet
 
             HashSet<string> hStringNames = new HashSet<string>(StringComparer.Ordinal);
             var s1 = Stopwatch.StartNew();
-            foreach (string s in _names)
+            foreach (string s in _uniqueNames)
             {
                 hStringNames.Contains(s);
             }
 
             s1.Stop();
-            //Console.WriteLine(((double)(s1.Elapsed.TotalMilliseconds * 10000000) /
-            //  1000000).ToString("0.000 ns"));
             Console.WriteLine(s1.Elapsed.TotalMilliseconds.ToString("0.000 ms"));
             Console.WriteLine();
             Console.WriteLine("Ends Here");
             Console.WriteLine("____________________________________");
             Console.WriteLine();
         }
+        
+        // This prints:
+        /*  ____________________________________
+            List Performance while checking Contains operation
+
+            0,002 ms
+
+            Ends Here
+
+            ____________________________________
+            HashSet Performance while checking Contains operation
+
+            0,004 ms
+
+            Ends Here
+            ____________________________________
+            */
+        
+
+
 
 
         static void Get_Remove_Performance_HashSet_vs_List()
@@ -344,8 +429,6 @@ namespace CS_Using_HashSet
             }
 
             s2.Stop();
-            //Console.WriteLine(((double)(s2.Elapsed.TotalMilliseconds * 10000000) /
-            // 1000000).ToString("0.000 ns"));
             Console.WriteLine(s2.Elapsed.TotalMilliseconds.ToString("0.000 ms"));
             Console.WriteLine();
             Console.WriteLine("Ends Here");
@@ -362,8 +445,6 @@ namespace CS_Using_HashSet
             }
 
             s1.Stop();
-            //Console.WriteLine(((double)(s1.Elapsed.TotalMilliseconds * 10000000) /
-            //  1000000).ToString("0.000 ns"));
             Console.WriteLine(s1.Elapsed.TotalMilliseconds.ToString("0.000 ms"));
             Console.WriteLine();
             Console.WriteLine("Ends Here");
